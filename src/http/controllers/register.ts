a82003f4d2bb97bdf24f65cd03c2ae2bc6a15f8a
Hikaro Eduardo <hikaroeduardobandeira@gmail.com>
2024-06-14 10:12:35 -0300

    fix: resolve create user duplicate

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -15,6 +15,16 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     const passwordHash = await hash(password, 6);
 
+    const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    });
+
+    if (userWithSameEmail) {
+        return reply.status(409).send();
+    }
+
     await prisma.user.create({
         data: {
             name,
