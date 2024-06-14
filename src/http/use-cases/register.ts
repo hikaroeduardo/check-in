6ba56c1b72af692762c5ba1f8b9a890c4cf74a01
Hikaro Eduardo <hikaroeduardobandeira@gmail.com>
2024-06-14 10:45:53 -0300

    refactor: refactor code for repository patterns

--- src/http/use-cases/register.ts
+++ src/http/use-cases/register.ts
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/prisma";
 import { hash } from "bcryptjs";
 
+import { PrismaUsersRepository } from "@/repositories/prisma-users-repository";
+
 interface RegisterUseCaseRequest {
     name: string;
     email: string;
@@ -24,11 +26,11 @@ export async function registerUseCase({
         throw new Error("E-mail already exists.");
     }
 
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash: passwordHash,
-        },
+    const prismaUsersRepository = new PrismaUsersRepository();
+
+    await prismaUsersRepository.create({
+        name,
+        email,
+        password_hash: passwordHash,
     });
 }
