fd388e6d69eb496046667296e28b577ca4bc30ca
Hikaro Eduardo <hikaroeduardobandeira@gmail.com>
2024-06-22 10:59:19 -0300

    feat: configure fastifyJwt

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import fastify from "fastify";
 import { ZodError } from "zod";
+import fastifyJwt from "@fastify/jwt";
+
+import { env } from "./env";
 
 import { appRoutes } from "./http/routes";
-import { env } from "./env";
 
 export const app = fastify();
 
+app.register(fastifyJwt, {
+    secret: env.JWT_SECRET,
+});
 app.register(appRoutes);
 
 app.setErrorHandler((error, _, reply) => {
