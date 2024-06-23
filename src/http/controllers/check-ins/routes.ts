f01ede1f0bd27d19ce223b9a9218f89a6980ea29
Hikaro Eduardo <hikaroeduardobandeira@gmail.com>
2024-06-23 17:57:07 -0300

    feat: add validate check in controller

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -3,9 +3,11 @@ import { FastifyInstance } from "fastify";
 import { verifyJWT } from "../../middlewares/verify-jwt";
 
 import { createCheckIns } from "./create-check-in";
+import { validateCheckIns } from "./validate-check-in";
 
 export async function checkInsRoutes(app: FastifyInstance) {
     app.addHook("onRequest", verifyJWT);
 
     app.post("/gyms/:gymId/check-ins", createCheckIns);
+    app.patch("/check-ins/:checkInId/validate", validateCheckIns);
 }
---
+++ src/http/controllers/check-ins/validate-check-in.ts
@@ -0,0 +1,23 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+
+import { z } from "zod";
+import { makeValidateCheckInUseCase } from "@/http/use-cases/factories/make-validate-ceck-in-use-case";
+
+export async function validateCheckIns(
+    request: FastifyRequest,
+    reply: FastifyReply
+) {
+    const validateCheckInParamsSchema = z.object({
+        checkInId: z.string().uuid(),
+    });
+
+    const { checkInId } = validateCheckInParamsSchema.parse(request.params);
+
+    const validateCheckInUseCase = makeValidateCheckInUseCase();
+
+    await validateCheckInUseCase.execute({
+        checkInId,
+    });
+
+    return reply.status(204).send();
+}
