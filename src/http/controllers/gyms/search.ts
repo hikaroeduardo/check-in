bb9d6750702f8ed883444d60711d735ed5566329
Hikaro Eduardo <hikaroeduardobandeira@gmail.com>
2024-06-23 17:19:26 -0300

    fix: resolve search gyms controller in search file

---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,24 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+
+import { z } from "zod";
+import { makeSearchGymsUseCase } from "@/http/use-cases/factories/make-search-gym-use-case";
+
+export async function searchGym(request: FastifyRequest, reply: FastifyReply) {
+    const searchGymsQuerySchema = z.object({
+        query: z.string(),
+        page: z.coerce.number().min(0).default(1),
+    });
+
+    const { query, page } = searchGymsQuerySchema.parse(request.body);
+
+    const searchGymUseCase = makeSearchGymsUseCase();
+
+    const { gyms } = await searchGymUseCase.execute({
+        query,
+        page,
+    });
+
+    return reply.status(200).send({
+        gyms,
+    });
+}
