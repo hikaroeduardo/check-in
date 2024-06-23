039a0f7b836704569ca61f5fb38d356fc1af15ff
Hikaro Eduardo <hikaroeduardobandeira@gmail.com>
2024-06-23 17:58:30 -0300

    feat: add routes to check-ins

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -4,9 +4,14 @@ import { verifyJWT } from "../../middlewares/verify-jwt";
 
 import { createCheckIns } from "./create-check-in";
 import { validateCheckIns } from "./validate-check-in";
+import { historyCheckIns } from "./history-check-in";
+import { metricsCheckIns } from "./metrics-check-ins";
 
 export async function checkInsRoutes(app: FastifyInstance) {
     app.addHook("onRequest", verifyJWT);
+
+    app.get("/check-ins/history", historyCheckIns);
+    app.get("/check-ins/metrics", metricsCheckIns);
 
     app.post("/gyms/:gymId/check-ins", createCheckIns);
     app.patch("/check-ins/:checkInId/validate", validateCheckIns);
