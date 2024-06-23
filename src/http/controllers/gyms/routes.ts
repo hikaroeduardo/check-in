6935ff98d79551b2c11f6e09c710a2f27e557f93
Hikaro Eduardo <hikaroeduardobandeira@gmail.com>
2024-06-23 17:23:35 -0300

    feat: add routes in gyms controller

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -2,6 +2,15 @@ import { FastifyInstance } from "fastify";
 
 import { verifyJWT } from "../../middlewares/verify-jwt";
 
+import { searchGym } from "./search";
+import { nearbyGym } from "./nearby";
+import { create } from "./create";
+
 export async function gymsRoutes(app: FastifyInstance) {
     app.addHook("onRequest", verifyJWT);
+
+    app.post("/gyms", create);
+
+    app.get("/gyms/search", searchGym);
+    app.get("/gyms/nearby", nearbyGym);
 }
