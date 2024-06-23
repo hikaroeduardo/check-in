b5a27cf3aab112e47b13f63371bbb2320198e3a5
Hikaro Eduardo <hikaroeduardobandeira@gmail.com>
2024-06-23 18:30:11 -0300

    feat: add refresh token route

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -5,10 +5,13 @@ import { verifyJWT } from "../../middlewares/verify-jwt";
 import { register } from "./register";
 import { authenticate } from "./authenticate";
 import { profile } from "./profile";
+import { refreshToken } from "./refresh-token";
 
 export async function usersRoutes(app: FastifyInstance) {
     app.post("/users", register);
     app.post("/sessions", authenticate);
+
+    app.patch("/token/refresh", refreshToken);
 
     // Precisam de autenticação
 
