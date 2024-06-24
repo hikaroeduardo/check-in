acd6466f89f880d28ded9230eb9130610fcb3ea1
Hikaro Eduardo <hikaroeduardobandeira@gmail.com>
2024-06-24 10:46:53 -0300

    refactor: refactor verify-user-role

--- src/http/middlewares/verify-user-role.ts
+++ src/http/middlewares/verify-user-role.ts
@@ -2,8 +2,8 @@ import { FastifyRequest, FastifyReply } from "fastify";
 
 type RoleProps = "ADMIN" | "MEMBER";
 
-export async function verifyUserRole(roleToVerify: RoleProps) {
-    return (request: FastifyRequest, reply: FastifyReply) => {
+export function verifyUserRole(roleToVerify: RoleProps) {
+    return async (request: FastifyRequest, reply: FastifyReply) => {
         const { role } = request.user;
 
         if (role !== roleToVerify) {
